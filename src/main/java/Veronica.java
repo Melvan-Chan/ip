0b55ab2ae77f80a85ecdc56a7793fe374f5ff0a8
Melvan-Chan <42329954+Melvan-Chan@users.noreply.github.com>
2025-01-28 16:13:17 +0800

    Echo

--- src/main/java/Veronica.java
+++ src/main/java/Veronica.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Veronica {
     public static void main(String[] args) {
         String greet = "____________________________________________________________\n"
-                + " Hello! I'm Veronica\n"
+                + " Hello! I'm Veronica. Tony Stark create me after Jarvis.\n"
+                + " A little bit about me is that my name means 'she who brings the victory'.\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
         System.out.println(greet);
+
+        Scanner sc = new Scanner(System.in); // Set up to read user input
+
+        while (true) {
+            String userInput = sc.nextLine(); // Read user input
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Veronica: Bye. Hope to see you again soon! ");
+                break;
+            }
+            System.out.println("Veronica: " + userInput);
+        }
+        sc.close();
     }
 }
