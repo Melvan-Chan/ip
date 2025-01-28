8c97b4a6d33620da176b64cce935dc58950906c6
Melvan-Chan <42329954+Melvan-Chan@users.noreply.github.com>
2025-01-28 15:40:47 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Veronica.java
@@ -0,0 +1,11 @@
+public class Veronica {
+    public static void main(String[] args) {
+        String greet = "____________________________________________________________\n"
+                + " Hello! I'm Veronica\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(greet);
+    }
+}
