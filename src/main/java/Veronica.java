4c30ed7f5b7bc7ebb0e10e550751a7c65a06a5d4
Melvan-Chan <42329954+Melvan-Chan@users.noreply.github.com>
2025-02-04 00:05:17 +0800

    Update code to Egyptian style and add test files.

--- src/main/java/Veronica.java
+++ src/main/java/Veronica.java
@@ -16,19 +16,22 @@ public class Veronica {
         int taskCount = 0;                      // Track number of tasks added
 
         while (true) {
-            String userInput = sc.nextLine(); // Read user input
+            String userInput = "";
+            if (sc.hasNextLine()) {
+                userInput = sc.nextLine(); // Read user input
+            } else {
+                break;
+            }
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Veronica: Bye. Hope to see you again soon! ");
                 break;
-            }
-            else if (userInput.equalsIgnoreCase("list")) {
+            } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
                 for (int i = 0; i < taskCount; ++i) {
                     System.out.println((i + 1) + ". " + (tasks[i]));
                 }
                 System.out.println("____________________________________________________________");
-            }
-            else if (userInput.startsWith("mark ")) {
+            } else if (userInput.startsWith("mark ")) {
                 int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
                 if (taskIndex >= 0 && taskIndex < taskCount) {
                     tasks[taskIndex].markAsComplete();
@@ -36,14 +39,12 @@ public class Veronica {
                     System.out.println("Veronica: Marking this task as completed!");
                     System.out.println(tasks[taskIndex]);
                     System.out.println("____________________________________________________________");
-                }
-                else {
+                } else {
                     System.out.println("____________________________________________________________");
                     System.out.println("Veronica: This task does not exist! Please try again.");
                     System.out.println("____________________________________________________________");
                 }
-            }
-            else if (userInput.startsWith("unmark ")) {
+            } else if (userInput.startsWith("unmark ")) {
                 int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                 if (taskIndex >= 0 && taskIndex < taskCount) {
                     tasks[taskIndex].markAsIncomplete();
@@ -51,14 +52,12 @@ public class Veronica {
                     System.out.println("Veronica: Unmarking this task as incomplete!");
                     System.out.println(tasks[taskIndex]);
                     System.out.println("____________________________________________________________");
-                }
-                else {
+                } else {
                     System.out.println("____________________________________________________________");
                     System.out.println("Veronica: This task does not exist! Please try again.");
                     System.out.println("____________________________________________________________");
                 }
-            }
-            else if (userInput.startsWith("remove ")) {
+            } else if (userInput.startsWith("remove ")) {
                 int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                 System.out.println("____________________________________________________________");
                 System.out.println("Veronica: Removing this task from the list!");
@@ -69,8 +68,7 @@ public class Veronica {
                     tasks[i] = tasks[i + 1];
                 }
                 taskCount--;
-            }
-            else if (userInput.startsWith("todo ")) {
+            } else if (userInput.startsWith("todo ")) {
                 String taskDescription = userInput.substring(5);
 
                 tasks[taskCount++] = new ToDo(taskDescription);
@@ -81,8 +79,7 @@ public class Veronica {
                 System.out.println("Now, you've got " + taskCount + " tasks in the list!");
                 System.out.println("____________________________________________________________");
 
-            }
-            else if (userInput.startsWith("deadline ")) {
+            } else if (userInput.startsWith("deadline ")) {
                 String[] parts = userInput.substring(9).split(" /by ");
 
                 if (parts.length == 2) {
@@ -92,8 +89,7 @@ public class Veronica {
                     System.out.println(tasks[taskCount - 1]);
                     System.out.println("Now, you've got " + taskCount + " tasks in the list!");
                     System.out.println("____________________________________________________________");
-                }
-                else {
+                } else {
                     System.out.println("____________________________________________________________");
                     System.out.println("Veronica: Invalid format. Use: deadline <task> /by <date>");
                     System.out.println("____________________________________________________________");
@@ -110,22 +106,19 @@ public class Veronica {
                     System.out.println(tasks[taskCount - 1]);
                     System.out.println("Now, you've got " + taskCount + " tasks in the list!");
                     System.out.println("____________________________________________________________");
-                }
-                else {
+                } else {
                     System.out.println("____________________________________________________________");
                     System.out.println("Veronica: Invalid format. Use: event <task> /from <start> /to <end>");
                     System.out.println("____________________________________________________________");
                 }
-            }
-            else {
+            } else {
                 if (taskCount < 100) {
                     tasks[taskCount++] = new Task(userInput);
 
                     System.out.println("____________________________________________________________");
                     System.out.println("Veronica: Added " + userInput + " to the list.");
                     System.out.println("____________________________________________________________");
-                }
-                else {
+                } else {
                     System.out.println("Veronica: Sorry, I'm unable to store more than 100 tasks!");
                 }
             }
