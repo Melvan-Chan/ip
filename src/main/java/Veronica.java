b72bae86cac072e6335e3edcb6019a0f99cd86c3
Melvan-Chan <42329954+Melvan-Chan@users.noreply.github.com>
2025-02-04 15:58:55 +0800

    Add task removal functionality

--- src/main/java/Veronica.java
+++ src/main/java/Veronica.java
@@ -6,6 +6,16 @@ public class Veronica {
                 _________________________________________________________________________________________________
                  Hello! I'm Veronica. Tony Stark create me after Jarvis.
                  A little bit about me is that my name means 'she who brings the victory'.
+                 
+                 Here's my command list:
+                 - todo <task>: Add's task to the list.
+                 - deadline <task> /by <date>: Add's deadline to the list with a due date.
+                 - event <task> /from <start> /to <end>: Add's event to the list with a start/end date..'
+                 - list: List's all the tasks in the list.
+                 - mark <no. of task>: Marks the task specified.
+                 - unmark <no. of task>: Unmarks the task specified.
+                 - remove <no. of task>: Removes the task specified.
+                 
                  What can I do for you?
                 _________________________________________________________________________________________________
                 """;
@@ -27,20 +37,20 @@ public class Veronica {
                     System.out.println("Veronica: Bye. Hope to see you again soon! ");
                     break;
                 } else if (userInput.equalsIgnoreCase("list")) {
-                    System.out.println("_________________________________________________________________________________________________");
+                    System.out.println("     _________________________________________________________________________________________________");
                     for (int i = 0; i < taskCount; ++i) {
-                        System.out.println((i + 1) + ". " + (tasks[i]));
+                        System.out.println("     " + (i + 1) + ". " + (tasks[i]));
                     }
-                    System.out.println("_________________________________________________________________________________________________");
+                    System.out.println("     _________________________________________________________________________________________________");
                 } else if (userInput.startsWith("mark ")) {
                     try {
                         int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
                         if (taskIndex >= 0 && taskIndex < taskCount) {
                             tasks[taskIndex].markAsComplete();
-                            System.out.println("_________________________________________________________________________________________________");
-                            System.out.println("Veronica: Marking this task as completed!");
-                            System.out.println(tasks[taskIndex]);
-                            System.out.println("_________________________________________________________________________________________________");
+                            System.out.println("     _________________________________________________________________________________________________");
+                            System.out.println("     Veronica: Marking this task as completed!");
+                            System.out.println("     " + tasks[taskIndex]);
+                            System.out.println("     _________________________________________________________________________________________________");
                         } else {
                             throw new VeronicaException("UHOH! I'm afraid that the task number does not exist! Please try again.");
                         }
@@ -52,14 +62,14 @@ public class Veronica {
                         int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                         if (taskIndex >= 0 && taskIndex < taskCount) {
                             tasks[taskIndex].markAsIncomplete();
-                            System.out.println("_________________________________________________________________________________________________");
-                            System.out.println("Veronica: Unmarking this task as incomplete!");
-                            System.out.println(tasks[taskIndex]);
-                            System.out.println("_________________________________________________________________________________________________");
+                            System.out.println("     _________________________________________________________________________________________________");
+                            System.out.println("     Veronica: Unmarking this task as incomplete!");
+                            System.out.println("     " + tasks[taskIndex]);
+                            System.out.println("     _________________________________________________________________________________________________");
                         } else {
-                            System.out.println("_________________________________________________________________________________________________");
-                            System.out.println("Veronica: This task does not exist! Please try again.");
-                            System.out.println("_________________________________________________________________________________________________");
+                            System.out.println("     _________________________________________________________________________________________________");
+                            System.out.println("     Veronica: This task does not exist! Please try again.");
+                            System.out.println("     _________________________________________________________________________________________________");
                         }
                     } catch (NumberFormatException e) {
                         throw new VeronicaException("UHOH! I'm afraid that the task number does not exist! Please try again.");
@@ -68,14 +78,15 @@ public class Veronica {
                     try {
                         int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
                         if (taskIndex >= 0 && taskIndex < taskCount) {
-                            System.out.println("_________________________________________________________________________________________________");
-                            System.out.println("Veronica: Removed: " + tasks[taskIndex]);
-                            System.out.println("_________________________________________________________________________________________________");
-
+                            System.out.println("     _________________________________________________________________________________________________");
+                            System.out.println("     Veronica: Removed " + tasks[taskIndex]);
                             for (int i = taskIndex; i < taskCount - 1; i++) {
                                 tasks[i] = tasks[i + 1];
                             }
                             taskCount--;
+                            System.out.println("     Veronica: Now, you've got " + taskCount + " tasks in the list!");
+                            System.out.println("     _________________________________________________________________________________________________");
+
                         } else {
                             throw new VeronicaException("OOPS!!! Task number does not exist.");
                         }
@@ -89,22 +100,22 @@ public class Veronica {
                     }
                     tasks[taskCount++] = new ToDo(taskDescription);
 
-                    System.out.println("_________________________________________________________________________________________________");
-                    System.out.println("Veronica: Alright, I've added this to the list.");
-                    System.out.println(tasks[taskCount - 1]);
-                    System.out.println("Now, you've got " + taskCount + " tasks in the list!");
-                    System.out.println("_________________________________________________________________________________________________");
+                    System.out.println("     _________________________________________________________________________________________________");
+                    System.out.println("     Veronica: Alright, I've added this to the list.");
+                    System.out.println("     " + tasks[taskCount - 1]);
+                    System.out.println("     Veronica: Now, you've got " + taskCount + " tasks in the list!");
+                    System.out.println("     _________________________________________________________________________________________________");
 
                 } else if (userInput.startsWith("deadline ")) {
                     String[] parts = userInput.substring(9).split(" /by ");
 
                     if (parts.length == 2) {
                         tasks[taskCount++] = new Deadline(parts[0], parts[1]);
-                        System.out.println("_________________________________________________________________________________________________");
-                        System.out.println("Veronica: Alright, I've added this to the list.");
-                        System.out.println(tasks[taskCount - 1]);
-                        System.out.println("Now, you've got " + taskCount + " tasks in the list!");
-                        System.out.println("_________________________________________________________________________________________________");
+                        System.out.println("     _________________________________________________________________________________________________");
+                        System.out.println("     Veronica: Alright, I've added this to the list.");
+                        System.out.println("     " + tasks[taskCount - 1]);
+                        System.out.println("     Veronica: Now, you've got " + taskCount + " tasks in the list!");
+                        System.out.println("     _________________________________________________________________________________________________");
                     } else {
                         throw new VeronicaException("UHOH! Invalid format detected. Use: deadline <task> /by <date>");
                     }
@@ -115,11 +126,11 @@ public class Veronica {
 
                     if (parts.length == 3) {
                         tasks[taskCount++] = new Event(parts[0], parts[1], parts[2]);
-                        System.out.println("_________________________________________________________________________________________________");
-                        System.out.println("Veronica: Alright, I've added this to the list.");
-                        System.out.println(tasks[taskCount - 1]);
-                        System.out.println("Now, you've got " + taskCount + " tasks in the list!");
-                        System.out.println("_________________________________________________________________________________________________");
+                        System.out.println("     _________________________________________________________________________________________________");
+                        System.out.println("     Veronica: Alright, I've added this to the list.");
+                        System.out.println("     " + tasks[taskCount - 1]);
+                        System.out.println("     Veronica: Now, you've got " + taskCount + " tasks in the list!");
+                        System.out.println("     _________________________________________________________________________________________________");
                     } else {
                         throw new VeronicaException("UHOH! Invalid format detected. Use: event <task> /from <start> /to <end>");
                     }
@@ -127,9 +138,9 @@ public class Veronica {
                     throw new VeronicaException("UHOH! I'm sorry, but I've no idea what you mean! Please try again.");
                 }
             } catch (VeronicaException errorMsg) {
-                System.out.println("_________________________________________________________________________________________________");
-                System.out.println("Veronica: " + errorMsg.getMessage());
-                System.out.println("_________________________________________________________________________________________________");
+                System.out.println("     _________________________________________________________________________________________________");
+                System.out.println("     Veronica: " + errorMsg.getMessage());
+                System.out.println("     _________________________________________________________________________________________________");
             }
         }
         sc.close();
